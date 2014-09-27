ac9b7d40b7b03d4cedbf369f0471948d7603f8c7
Federico Cosentino <fed.cosentino@gmail.com>
2014-09-27 23:25:06 +0200

    QR Code Intent

--- client/src/it/mrmodd/registrationform/MainFragment.java
+++ client/src/it/mrmodd/registrationform/MainFragment.java
@@ -1,9 +1,13 @@
 package it.mrmodd.registrationform;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,6 +44,42 @@ public class MainFragment extends Fragment {
 		editTextID = (EditText) v.findViewById(R.id.editText_id);
 		editTextName = (EditText) v.findViewById(R.id.editText_name);
 		
+		buttonQR.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				try {
+
+				    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+				    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+				    startActivityForResult(intent, 0);
+
+				} catch (Exception e) {
+
+				    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+				    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+				    startActivity(marketIntent);
+
+				}
+			}
+			
+		});
+		
 		return v;
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {           
+	    super.onActivityResult(requestCode, resultCode, data);
+	    if (requestCode == 0) {
+
+	        if (resultCode == Activity.RESULT_OK) {
+	        	textViewCurrentID.setText(getActivity().getString(R.string.text_currentID) + " " + data.getStringExtra("SCAN_RESULT"));
+	        }
+	        if(resultCode == Activity.RESULT_CANCELED){
+	            //handle cancel
+	        }
+	    }
 	}
 }
