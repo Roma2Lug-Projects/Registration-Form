412b9c11856dd0aa860ed2ed5e9eca8916335b21
Federico Cosentino <fed.cosentino@gmail.com>
2014-09-28 17:16:05 +0200

    Beta version of the client, semantic search not working yet

--- client/src/it/mrmodd/registrationform/MainFragment.java
+++ client/src/it/mrmodd/registrationform/MainFragment.java
@@ -1,10 +1,29 @@
 package it.mrmodd.registrationform;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,12 +31,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainFragment extends Fragment {
 	private Button buttonQR;
 	private Button buttonID;
 	private Button buttonName;
 	private Button buttonAccept;
+	private Button buttonRemove;
 	private Button buttonCancel;
 	
 	private TextView textViewCurrentID;
@@ -25,6 +46,8 @@ public class MainFragment extends Fragment {
 	private EditText editTextID;
 	private EditText editTextName;
 	
+	private Integer currentID = 0;
+	private JSONObject currentJSON = null;
 	
 	
 	@Override
@@ -37,6 +60,7 @@ public class MainFragment extends Fragment {
 		buttonID = (Button) v.findViewById(R.id.button_id);
 		buttonName = (Button) v.findViewById(R.id.button_name);
 		buttonAccept = (Button) v.findViewById(R.id.button_accept);
+		buttonRemove = (Button) v.findViewById(R.id.button_remove);
 		buttonCancel = (Button) v.findViewById(R.id.button_cancel);
 		
 		textViewCurrentID = (TextView) v.findViewById(R.id.text_currentID);
@@ -59,9 +83,92 @@ public class MainFragment extends Fragment {
 
 				    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
 				    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+				    
+				    cleanScreen();
+				    
 				    startActivity(marketIntent);
 
 				}
+			}
+			
+		});
+		
+		buttonID.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String id = editTextID.getText().toString();
+				
+				if (id.length() == 0) {
+					Toast.makeText(getActivity(), R.string.error_empty_field, Toast.LENGTH_SHORT).show();
+					return;
+				}
+				if (!id.matches("[0-9]+")) {
+					Toast.makeText(getActivity(), R.string.error_invalid_field, Toast.LENGTH_SHORT).show();
+					return;
+				}
+				
+				editTextID.setText("");
+				editTextName.setText("");
+				
+				cleanScreen();
+				
+				currentID = Integer.parseInt(id);
+        		new RetrieveDetails().execute();
+				
+			}
+			
+		});
+		
+		buttonName.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				cleanScreen();
+
+				//TODO
+			}
+			
+		});
+		
+		buttonAccept.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				if (currentID == 0) {
+					Toast.makeText(getActivity(), R.string.error_no_id_selected, Toast.LENGTH_SHORT).show();
+					return;
+				}
+				
+				new CheckInID().execute();
+			}
+			
+		});
+		
+		buttonRemove.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				if (currentID == 0) {
+					Toast.makeText(getActivity(), R.string.error_no_id_selected, Toast.LENGTH_SHORT).show();
+					return;
+				}
+				
+				new CheckOutID().execute();
+			}
+			
+		});
+		
+		buttonCancel.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				cleanScreen();
+				
 			}
 			
 		});
@@ -75,11 +182,258 @@ public class MainFragment extends Fragment {
 	    if (requestCode == 0) {
 
 	        if (resultCode == Activity.RESULT_OK) {
-	        	textViewCurrentID.setText(getActivity().getString(R.string.text_currentID) + " " + data.getStringExtra("SCAN_RESULT"));
+	        	String qr = data.getStringExtra("SCAN_RESULT");
+	        	if (qr.matches("[0-9]+")) {
+	        		
+	        		currentID = Integer.parseInt(qr);
+	        		new RetrieveDetails().execute();
+	        		
+	        	}
+	        	else
+	        		Toast.makeText(getActivity(), R.string.error_qr_content, Toast.LENGTH_SHORT).show();
 	        }
 	        if(resultCode == Activity.RESULT_CANCELED){
 	            //handle cancel
 	        }
 	    }
+	}
+	
+	private void cleanScreen() {
+		currentID = 0;
+		currentJSON = null;
+		textViewCurrentID.setText(R.string.text_empty);
+		editTextID.setText("");
+		editTextName.setText("");
+	}
+	
+	private HttpURLConnection enstablishConnection(String method, String file) {
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		
+		String url_pref = pref.getString(getActivity().getString(R.string.pref_key_server_url), "");
+		String credentials = pref.getString(getActivity().getString(R.string.pref_key_username), "")
+								+ ":"
+								+ pref.getString(getActivity().getString(R.string.pref_key_password), "");
+		URL url;
+		HttpURLConnection connection = null;
+		
+		try {
+			url = new URL(url_pref + file);
+			
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod(method);
+			
+			if (method.equalsIgnoreCase("POST") || method.equalsIgnoreCase("PUT"))
+				connection.setDoOutput(true);
+			connection.setDoInput(true);
+			
+			connection.setRequestProperty("Authorization", "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.DEFAULT));
+			
+		} catch (MalformedURLException e) {
+			Toast.makeText(getActivity(), R.string.error_malformed_url, Toast.LENGTH_SHORT).show();
+			return null;
+		} catch (ProtocolException e) {
+			Toast.makeText(getActivity(), R.string.error_internal_error, Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			Toast.makeText(getActivity(), R.string.error_connection, Toast.LENGTH_SHORT).show();
+			return null;
+		}
+		
+		return connection;
+	}
+	
+	private String fetchJSON(HttpURLConnection conn) throws IOException {
+		InputStream content = conn.getInputStream();
+		BufferedReader in = new BufferedReader (new InputStreamReader(content));
+		String line;
+		String json = "";
+		while ((line = in.readLine()) != null) {
+			json += line;
+		}
+		content.close();
+		in.close();
+		
+		return json;
+	}
+	
+	private void writeJSON(HttpURLConnection conn, String json) throws IOException {
+		
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("Content-Length", "" + json.length());
+		
+		OutputStream os = conn.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
+		dos.writeBytes(json);
+		dos.flush();
+		dos.close();
+		os.close();
+		
+	}
+	
+	class RetrieveDetails extends AsyncTask<Void, String, String> {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			String newtext = null;
+			
+			HttpURLConnection conn = enstablishConnection("GET", currentID.toString() + "/");
+			if (conn != null) {
+				
+				try {
+					if (conn.getResponseCode() == 200) {
+					
+						String json = fetchJSON(conn);
+
+						//Parse JSON
+						currentJSON = new JSONObject(json);
+						String first_name = currentJSON.getString("first_name");
+						String last_name = currentJSON.getString("last_name");
+						String email = currentJSON.getString("email");
+						Boolean morning = currentJSON.getBoolean("participate_morning");
+						Boolean afternoon = currentJSON.getBoolean("participate_afternoon");
+						String checkin = (!currentJSON.getString("check_in").matches("null")) ? currentJSON.getString("check_in") : "--";
+						String comments = currentJSON.getString("comments");
+
+						newtext = getActivity().getString(R.string.text_currentID) + " " + currentID.toString() + "\n";
+						newtext += getActivity().getString(R.string.text_name) + " " + first_name + "\n";
+						newtext += getActivity().getString(R.string.text_lastname) + " " + last_name + "\n";
+						newtext += getActivity().getString(R.string.text_email) + " " + email + "\n";
+						newtext += getActivity().getString(R.string.text_morning) + " " + morning + "\n";
+						newtext += getActivity().getString(R.string.text_afternoon) + " " + afternoon + "\n";
+						newtext += getActivity().getString(R.string.text_check_in) + " " + checkin + "\n\n";
+						newtext += getActivity().getString(R.string.text_comments) + "\n" + comments;
+			            
+					}
+					else {
+						publishProgress(getActivity().getString(R.string.error_server_code) + "\n" + conn.getResponseCode() + ": " + conn.getResponseMessage());
+					}
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					publishProgress(getActivity().getString(R.string.error_invalid_response));
+				} finally {
+					conn.disconnect();
+				}
+				
+			}
+			
+			return newtext;
+		}
+		
+		@Override
+		protected void onProgressUpdate(String... values) {
+			Toast.makeText(getActivity(), values[0], Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			if (result != null)
+				textViewCurrentID.setText(result);
+		}
+	}
+	
+	class CheckInID extends AsyncTask<Void, String, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			HttpURLConnection conn = enstablishConnection("PUT", currentID.toString() + "/");
+			if (conn != null && currentJSON != null) {
+				
+				try {
+					
+					if (!currentJSON.getString("check_in").matches("null"))
+						publishProgress(getActivity().getString(R.string.error_already_checked_in));
+					else {
+						String date = new java.sql.Timestamp(new Date().getTime()).toString();
+						currentJSON.put("check_in", date);
+						
+						currentJSON.put("comments", null);
+						
+						writeJSON(conn, currentJSON.toString());
+						
+						if (conn.getResponseCode() == 200) {
+							publishProgress(conn.getResponseMessage());
+							fetchJSON(conn);
+						}
+						else
+							publishProgress(getActivity().getString(R.string.error_server_code) + "\n" + conn.getResponseCode() + ": " + conn.getResponseMessage());
+					}
+					
+				} catch (JSONException e) {
+					publishProgress(getActivity().getString(R.string.error_internal_error));
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					conn.disconnect();
+				}
+				
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(String... values) {
+			Toast.makeText(getActivity(), values[0], Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			cleanScreen();
+		}
+	}
+	
+	class CheckOutID extends AsyncTask<Void, String, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			
+			HttpURLConnection conn = enstablishConnection("PUT", currentID.toString() + "/");
+			if (conn != null && currentJSON != null) {
+				
+				try {
+					
+					if (currentJSON.getString("check_in").matches("null"))
+						publishProgress(getActivity().getString(R.string.error_already_checked_out));
+					else {
+						currentJSON.put("check_in", JSONObject.NULL);
+
+						currentJSON.put("comments", null);
+						
+						writeJSON(conn, currentJSON.toString());
+						
+						if (conn.getResponseCode() == 200) {
+							publishProgress(conn.getResponseMessage());
+							fetchJSON(conn);
+						}
+						else
+							publishProgress(getActivity().getString(R.string.error_server_code) + "\n" + conn.getResponseCode() + ": " + conn.getResponseMessage());
+					}
+					
+				} catch (JSONException e) {
+					publishProgress(getActivity().getString(R.string.error_internal_error));
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					conn.disconnect();
+				}
+				
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(String... values) {
+			Toast.makeText(getActivity(), values[0], Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			cleanScreen();
+		}
 	}
 }
